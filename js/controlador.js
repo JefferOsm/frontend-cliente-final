459bf02d847517fddd6192e3d44edb833854ae88
Jeffer <manymartinez83@gmail.com>
2022-12-02 19:45:20 -0600

    Integracion al backend y funcionamiento del CRUD

--- js/controlador.js
+++ js/controlador.js
@@ -1,125 +1,46 @@
-var restaurantes=[
-    {
-        id: 1,
-        nombreRestaurante: 'Burger King',
-        icono: 'img/logos/Burger_King_logo_(1999).svg.png',
-        descripcion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.',
-        calificacion: 4,
-        categorias:[
-            {
-                nombreCategoria: 'Bebidas',
-                tipos: 20,
-                imagenCategoria:'img/categorias/Refrescos.webp',
-                producto:[
-                    {
-                        nombreProducto: 'Coca Cola',
-                        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.',
-                        precio: 200,
-                        imgProducto:'img/producto/coca-cola.webp',
-                    },
-                    {
-                        nombreProducto: 'Pepsi',
-                        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.',
-                        precio: 400,
-                        imgProducto:'img/producto/pepsi.webp',
-                    },
-                ],
-            },
-            {
-                nombreCategoria: 'Hamburguesas',
-                tipos: 30,
-                imagenCategoria:'img/categorias/Hamburguesa.jpg',
-                producto:[
-                    {
-                        nombreProducto: 'Whooper',
-                        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.',
-                        precio: 300,
-                        imgProducto:'img/producto/1.webp',
-                    },
-                    {
-                        nombreProducto: 'Bacon King',
-                        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.',
-                        precio: 400,
-                        imgProducto:'img/producto/2.jpg',
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        id: 2,
-        nombreRestaurante: 'Bigos',
-        icono: 'img/logos/bigos.png',
-        descripcion:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.',
-        calificacion: 3,
-        categorias:[
-            {
-                nombreCategoria: 'Bebidas',
-                tipos: 20,
-                imagenCategoria:'img/categorias/Refrescos.webp',
-                producto:[
-                    {
-                        nombreProducto: 'Coca Cola',
-                        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.',
-                        precio: 200,
-                        imgProducto:'img/producto/coca-cola.webp',
-                    },
-                    {
-                        nombreProducto: 'Pepsi',
-                        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.',
-                        precio: 400,
-                        imgProducto:'img/producto/pepsi.webp',
-                    },
-                ],
-            },
-            {
-                nombreCategoria: 'Papas',
-                tipos: 30,
-                imagenCategoria:'img/categorias/Hamburguesa.jpg',
-                producto:[
-                    {
-                        nombreProducto: 'Whooper',
-                        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.',
-                        precio: 300,
-                        imgProducto:'img/producto/1.webp',
-                    },
-                    {
-                        nombreProducto: 'Bacon King',
-                        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec metus commodo, convallis leo in.',
-                        precio: 400,
-                        imgProducto:'img/producto/2.jpg',
-                    },
-                ],
-            },
-        ],
-    },
-];
 
-function generarRestaurantes(){
+let empresas;
+let categories;
+let product;
+let detalleProducto;
+
+
+//Funcionalidad par obtener Restaurantes
+const cargarRestaurantes= async()=>{
+    const respuesta= await fetch('http://localhost:3000/empresas',{
+        method: 'GET',
+
+    })
+    empresas= await respuesta.json();
+    console.log("Empresas", empresas) ;
+}
+
+
+const generarRestaurantes = ()=>{
     //LIMPIAR
     document.getElementById('restaurantes').innerHTML= '';
     document.getElementById('contenedor-categorias').style.display='none';
     document.getElementById('contenedorProductos').style.display='none';
     document.getElementById('contenedor-detalle').style.display='none';
-    restaurantes.forEach(function(rest, i){
+    empresas.forEach(function(empresa){
         let estrellas= '';
-        for(let i=0; i<rest.calificacion; i++){
+        for(let i=0; i<empresa.calificacion; i++){
             estrellas+= ' <i class="fa-solid fa-star"></i>';
         }
-        for(let i=0; i<(5-rest.calificacion); i++){
+        for(let i=0; i<(5-empresa.calificacion); i++){
             estrellas+= '<i class="fa-regular fa-star"></i>';
         }
 
         document.getElementById('restaurantes').innerHTML += 
         ` <div class="tarjeta sombra">
-        <img class="tarjeta-logo sombra" src="${rest.icono}" alt="Burger King">
+        <img class="tarjeta-logo sombra" src="${empresa.icono}" alt="Burger King">
         <div class="descripcion">
-            <div class="descripcion-nombre">${rest.nombreRestaurante}</div>
+            <div class="descripcion-nombre">${empresa.nombreRestaurante}</div>
             <div class="estrellas my-2">
                 ${estrellas}
                 <br>
             </div>
-            <btn onclick="generarCategorias('${i}')" ><i class="fa-solid fa-circle-right"></i></btn>
+            <btn onclick="cargarCategorias('${empresa._id}')" ><i class="fa-solid fa-circle-right"></i></btn>
         </div>
     </div>`;
 
@@ -127,10 +48,25 @@ function generarRestaurantes(){
 
 }
 
-generarRestaurantes()
+cargarRestaurantes().then(()=>{
+    generarRestaurantes();
+})
 
+//Fin Restaurantes
 
-function generarCategorias(indice){
+//Funcionalidad Obtener y Mostrar Categorias
+
+const cargarCategorias= async(idEmpresa)=>{
+    const respuesta= await fetch(`http://localhost:3000/empresas/${idEmpresa}/categorias`,{
+        method: 'GET',
+
+    })
+    categories= await respuesta.json();
+    console.log("Categorias", categories) ;
+    generarCategorias(categories);
+}
+
+function generarCategorias(restaurante){
     //Limpiar Restaurantes
     document.getElementById('restaurantes').style.display='none';
     document.getElementById('restaurantes-nav').style.display='none';
@@ -141,27 +77,38 @@ function generarCategorias(indice){
     document.getElementById('contenedor-detalle').style.display='none';
 
 
-    document.getElementById('logoCategoria').innerHTML+=` <img class="restaurante-logo" src="${restaurantes[indice].icono}" alt="Logo">`;
+    document.getElementById('logoCategoria').innerHTML+=` <img class="restaurante-logo" src="${restaurante.icono}" alt="Logo">`;
     document.getElementById('contenedor-categorias').style.display='block';
-    restaurantes[indice].categorias.forEach(function(cat, i){
+    restaurante.categorias.forEach(function(categoria){
         document.getElementById('categorias').innerHTML+=
         ` <div class="categoria-tarjeta sombra">
-        <div> <img  class="categoria-tarteja-img sombra" src="${cat.imagenCategoria}" alt=""> </div>
+        <div> <img  class="categoria-tarteja-img sombra" src="${categoria.imagenCategoria}" alt=""> </div>
         <div class="categoria-tarjeta-texto">
             <div class="categoria-textoDescripcion">
-                <h3>${cat.nombreCategoria}</h3>
-                <p>${cat.tipos} Tipos</p>
+                <h3>${categoria.nombreCategoria}</h3>
+                <p>${categoria.tipos} Tipos</p>
             </div>
-            <div class="categoria-siguiente"><btn onclick="generarProductos('${indice}','${i}')"><i class="fa-solid fa-circle-right"></i></btn></div>
+            <div class="categoria-siguiente"><btn onclick="cargarProductos('${categoria._id}','${restaurante._id}')"><i class="fa-solid fa-circle-right"></i></btn></div>
         </div>
     </div>`;
     });
 }
 
+//Fin Categorias
 
-function generarProductos(rest, cat){
-    let productos= restaurantes[rest].categorias[cat];
-    console.log(productos.producto);
+
+//Funcionalidad para obtener y Mostrar Productos
+const cargarProductos = async (idCategoria, idEmpresa)=>{
+    const respuesta= await fetch(`http://localhost:3000/categorias/${idCategoria}/productos`,{
+        method: 'GET',
+    })
+    product= await respuesta.json()
+    generarProductos(product, idEmpresa);
+    console.log('productos', product);
+}
+
+
+ function generarProductos(categoria,empresa){
     //LIMPIAR
     document.getElementById('contenedor-categorias').style.display='none';
     document.getElementById('restaurantes').style.display='none';
@@ -172,30 +119,38 @@ function generarProductos(rest, cat){
     document.getElementById('restaurantes-nav').style.display='block';
     document.getElementById('restaurantes-nav').innerHTML='';
     document.getElementById('restaurantes-nav').innerHTML+=
-    `<div><btn onclick="generarCategorias('${cat}')"   style="color:#CC7952 ;"><i class="fa-solid fa-circle-arrow-left"></i></btn></div>
-    <div style="color:black ;">${productos.nombreCategoria}</div>
+    `<div><btn onclick="cargarCategorias('${empresa}')"   style="color:#CC7952 ;"><i class="fa-solid fa-circle-arrow-left"></i></btn></div>
+    <div style="color:black ;">${categoria.nombreCategoria}</div>
     <button type="button" class="btn btn-cart" data-bs-toggle="modal" data-bs-target="#modal-carrito"><i class="fa-solid fa-cart-shopping" ></i></button>`;
     document.getElementById('restaurantes-nav').style.display='flex';
     //RENDERIZAR
     document.getElementById('contenedorProductos').style.display='block';
-    productos.producto.forEach(function(prod, i){
+    categoria.productos.forEach(function(producto){
         document.getElementById('contenedorProductos').innerHTML+=
         ` <div class="producto-tarjeta">
             <div class="producto-detalle">
-                <div class="producto-nombre">${prod.nombreProducto}</div>
-                <div class="producto-precio">L.${prod.precio}</div>
+                <div class="producto-nombre">${producto.nombreProducto}</div>
+                <div class="producto-precio">L.${producto.precio}</div>
             </div>
-            <div ><a onclick="generarDetalleProducto('${rest}','${cat}','${i}')"><img class="producto-imagen" src="${prod.imgProducto}" alt="..."></a></div>
+            <div ><a onclick="cargarDetalleProducto('${producto._id}','${categoria._id}','${empresa}','${categoria.nombreCategoria}')"><img class="producto-imagen" src="${producto.imgProducto}" alt="..."></a></div>
          </div>`;
 
     });
+}
+//Fin Productos
 
-
+const cargarDetalleProducto= async(idProducto, idCategoria, idEmpresa,nombreCategoria)=>{
+    const respuesta= await fetch(`http://localhost:3000/productos/${idProducto}`,{
+        method: 'GET'
+    })
+    detalleProducto= await respuesta.json();
+    generarDetalleProducto(detalleProducto,idCategoria,idEmpresa,nombreCategoria)
+    console.log(detalleProducto);
+    
 }
 
-function generarDetalleProducto(empresa, categoria,prod){
-    let detalle= restaurantes[empresa].categorias[categoria].producto[prod];
-    console.log(detalle.descripcion);
+
+function generarDetalleProducto(producto, categoria, empresa,header){
     //LIMPIAR
     document.getElementById('contenedor-categorias').style.display='none';
     document.getElementById('restaurantes').style.display='none';
@@ -208,16 +163,16 @@ function generarDetalleProducto(empresa, categoria,prod){
     document.getElementById('restaurantes-nav').style.display='block';
     document.getElementById('restaurantes-nav').innerHTML='';
     document.getElementById('restaurantes-nav').innerHTML+=
-    `<div><btn onclick="generarProductos('${empresa}','${categoria}')"   style="color:#CC7952 ;"><i class="fa-solid fa-circle-arrow-left"></i></btn></div>
-    <div style="color:black ;">${restaurantes[empresa].categorias[categoria].nombreCategoria}</div>
+    `<div><btn onclick="cargarProductos('${categoria}','${empresa}')"   style="color:#CC7952 ;"><i class="fa-solid fa-circle-arrow-left"></i></btn></div>
+    <div style="color:black ;">${header}</div>
     <button type="button" class="btn btn-cart" data-bs-toggle="modal" data-bs-target="#modal-carrito"><i class="fa-solid fa-cart-shopping" ></i></button>`;
     document.getElementById('restaurantes-nav').style.display='flex';
 
         //Generar Detalle
     document.getElementById('contenedor-detalle').innerHTML+=
-    `<img class="producto-d-img" src="${detalle.imgProducto}" alt="...">
-    <h2 class="my-3">${detalle.nombreProducto}</h2>
-    <p class="detalle-descripcion">${detalle.descripcion}</p>
+    `<img class="producto-d-img" src="${producto.imgProducto}" alt="...">
+    <h2 class="my-3">${producto.nombreProducto}</h2>
+    <p class="detalle-descripcion">${producto.descripcion}</p>
     
     <div class="detalle-formulario">
         <div class="detalle-cantidad">
@@ -225,7 +180,7 @@ function generarDetalleProducto(empresa, categoria,prod){
             <input type="number" class="form-control form-control-sm detalle-cantidad-n">
         </div>
         
-        <h2 class="detalle-precio">L.${detalle.precio}</h2>
+        <h2 class="detalle-precio">L.${producto.precio}</h2>
     </div>
 
     <div class="contenedor-botones ">
